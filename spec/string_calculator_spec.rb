8a16b837dc1ede98e5d8f93a481ca220ef0b0205
sumit <sumit-vishvakarma>
2025-09-04 01:34:54 -0700

    test: added spec for empty string returns 0

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -2,6 +2,11 @@
 require_relative '../string_calculator'
 
 RSpec.describe StringCalculator do
-	describe ".add" do
-	end
+  describe ".add" do
+    context "given empty string" do
+      it "returns 0" do
+        expect(StringCalculator.add("")).to eq(0)
+      end
+    end
+  end
 end
