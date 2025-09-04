5d52f7b21197fef5d5a8d18cedc5708a9eebd9b8
sumit <sumit-vishvakarma>
2025-09-04 01:42:33 -0700

    test: added spec for single number

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -8,5 +8,11 @@ RSpec.describe StringCalculator do
         expect(StringCalculator.add("")).to eq(0)
       end
     end
+
+		context "given '1' " do
+			it "returns 1" do
+				expect(StringCalculator.add("1")).to eq(1) 
+			end 
+		end
   end
 end
