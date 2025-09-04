0b5c31129e5e2729852e853169f35e1a1ffa0f32
sumit <sumit-vishvakarma>
2025-09-04 02:19:57 -0700

    feat: parse custom delimiter from header add method

--- string_calculator.rb
+++ string_calculator.rb
@@ -4,8 +4,15 @@ class StringCalculator
 
 		def add(numbers)
 			return 0 if numbers.empty?
-			numbers.split(/,|\n/).map(&:to_i).sum
+
+				if numbers.start_with?("//")
+					delimiter, numbers = numbers.split("\n", 2)
+					delimiter = delimiter[2..]
+					numbers.split(delimiter).map(&:to_i).sum
+				else
+					numbers.split(/,|\n/).map(&:to_i).sum
+				end
+			end
 		end
-  end
 end
 
