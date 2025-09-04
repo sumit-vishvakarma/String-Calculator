5266fdfd1da75e7636d17bdf01a0d8b1ba067ada
sumit <sumit-vishvakarma>
2025-09-04 01:53:09 -0700

    feat: sum two comma-separated numbers

--- string_calculator.rb
+++ string_calculator.rb
@@ -1,9 +1,11 @@
 # string_calculator.rb
 class StringCalculator
   class << self
+
 		def add(numbers)
 			return 0 if numbers.empty?
-			return numbers.to_i
+			numbers.split(',').map(&:to_i).sum
 		end
+
   end
 end
